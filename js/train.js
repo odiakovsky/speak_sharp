621cbbf56b24f6db14e0dfba93a314ae1f34d671
Oleg Dyakovskii <odiakovsky@gmail.com>
2023-04-11 21:40:40 +0300

    footer

--- js/train.js
+++ js/train.js
@@ -23,6 +23,7 @@ cards.forEach(card => {
 //Выбор тренеровки
 
 
+//Раскрытие фразы из мути
 const phrase = document.querySelector(".translation-phrase");
 const mute = document.querySelector(".mute");
 const h2 = document.querySelector(".translation-phrase h2");
@@ -31,3 +32,16 @@ phrase.addEventListener("click", () => {
   mute.style.display = "none";
   h2.style.display = "block";
 });
+//Раскрытие фразы из мути
+
+
+//Слайдер
+var slider = document.getElementById("slider");
+var output = document.getElementById("value");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+//Слайдер
+
